6f45b457fb350736b046f9cb38ce38525c4ad0fb
Thanh Tam <thanhtamtk234@gmail.com>
2019-07-31 17:13:54 +0700

    3107-hoanthanh

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   get '/welcome' , to:'pages#welcome'
   get '/contact', to:'pages#contact'
+  get 'book/list'
+  get 'book/new'
+  post 'book/create'
+  patch 'book/update'
+  get 'book/list'
+  get 'book/show'
+  get 'book/edit'
+  get 'book/delete'
+  get 'book/update'
+  get 'book/show_subjects'
 end
